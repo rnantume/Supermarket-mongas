5744c8298839c0e8c718972a5f81209559ca44ec
johnkegz <johnkl24@gmail.com>
2018-08-27 01:59:25 -0700

    manager add a new stock item, update stock and also do what till atendant do[#160054623]

--- manager.py
+++ manager.py
@@ -1,12 +1,25 @@
 import stock_item
-class Manager(Users,TillAtendant.StockItem):
+class Manager(Users, TillAtendant, StockItem):
     def add_stock_item(self):
-         item_name = input("item_name")
-         item_code = input("item_code")
-         purchase_price = input("purchase_price")
-         sale_price = input("sell_price")
-        stock_item = StockItem(item_name, item_code, purchase_price, sale_price)
-        stock_item.add_item() 
-    def update_stock_balance():
-        pass      
+        item_name = input("item_name")
+        item_code = input("item_code")
+        purchase_price = input("purchase_price")
+        sale_price = input("sell_price")
+        stock_item = StockItem()
+        stock_item.add_item(item_name, item_code, purchase_price, sale_price) 
+    def update_stock_balance(self):
+        item_name = input("item_name")
+        item_code = input("item_code")
+        purchase_price = input("purchase_price")
+        sale_price = input("sell_price")
+        update_item = StockItem()
+        update_item.update_item(item_name, item_code, purchase_price, sale_price)
+    def search(self):
+        till_attendant = TillAtendant()
+        till_attendant.search_items()
+    def sell(self):
+        till_attendant = TillAtendant()
+        till_attendant.sell_item_to_customers()
+
+
     
