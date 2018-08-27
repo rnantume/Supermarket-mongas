025bfc370d230fb0812cc5de3874b24572958377
Sanya Kenneth <sanyakenneth@gmail.com>
2018-08-27 12:27:57 +0300

    modify owner constructor

--- owner.py
+++ owner.py
@@ -1,6 +1,8 @@
 import users
 
 class Owner(Users):
+    def __init__(self,username,password,role):
+        super.(Owner, self).__init__(self,self,username,password,role)
     
     def view_daily_sales(self):
         pass
