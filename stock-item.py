1828c51b0718cac045f74dd25b45c2954df92154
asiimwemark <mugishasiimwe@gmail.com>
2018-08-27 11:47:24 +0300

    Add stock items

---
+++ stock-item.py
@@ -0,0 +1,12 @@
+items = []
+class StockItem:
+    def ___init__(self,item_name, item_code, purchase_price, sale_price):
+        self.item_name = item_name
+        self.item_code = item_code
+        self.purchase_price = purchase_price
+        self.sale_price = sale_price
+
+    def add_item(self,time_stamp, username ):
+        pass
+    def update_item(self):
+        pass
